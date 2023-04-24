0776e12490d8fba0f1b6cba286936ddec1ebc4da
roma4ka0 <111571102+roma4ka0@users.noreply.github.com>
2023-04-24 16:18:05 +0300

    2

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open("static")
+        .then(function(caches) {
+            console.log("precaching");
+            caches.add('/index.html');
+            caches.add('/');
+        })
+    )
+  });
+  self.addEventListener('activate', function(event) {
+ console.log('[Service Worker] Activating Service Worker ...', event);
+ return self.clients.claim();
+ });
+   self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response)
+                return response;
+                else
+                    return fetch(event.response);
+            }
+        )
+    );
+  });
+ 
