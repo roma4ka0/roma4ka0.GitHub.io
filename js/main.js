d95fc5f2e7eca39a498b47b69f8bf585642369ee
roma4ka0 <111571102+roma4ka0@users.noreply.github.com>
2023-04-25 17:33:31 +0300

    2

--- js/main.js
+++ js/main.js
@@ -129,3 +129,51 @@ var Application = new Vue({
       },
     },
   });
+
+
+  const cap_btn = document.getElementById("cap_btn");
+  const cat_btn = document.getElementById("cat_btn");
+  const dog_btn = document.getElementById("dog_btn");
+  const cap_result = document.getElementById("cap_result");
+  const cat_result = document.getElementById("cat_result");
+  const dog_result = document.getElementById("dog_result");
+  
+  cap_btn.addEventListener("click", getRandomCap);
+  cat_btn.addEventListener("click", getRandomCat);
+  dog_btn.addEventListener("click", getRandomDog);
+  
+  function getRandomCat() {
+    fetch("https://api.thecatapi.com/v1/images/search")
+      .then((res) => res.json())
+      .then((data) => {
+        cat_result.innerHTML = `<img src=${data[0].url} alt="cat" />`;
+      });
+  }
+  
+  function getRandomDog() {
+    fetch("https://random.dog/woof.json")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.url.includes(".mp4")) {
+          getRandomDog();
+        } else {
+          dog_result.innerHTML = `<img src=${data.url} alt="dog" />`;
+        }
+      });
+  }
+  function getRandomCap() {
+    fetch("https://restcountries.com/v3.1/region/europe")
+      .then((resolve) => resolve.json())
+      .then((data) => {
+        data.forEach((country) => {
+          console.log(country.capital[0]);
+        });
+      });
+  } 
+  fetch("https://restcountries.com/v3.1/region/europe")
+  .then((resolve) => resolve.json())
+  .then((data) => {
+    data.forEach((element) => {
+      document.getElementById("Ul-Capitals").innerHTML += `<li>${element.capital[0]}`;
+    });
+  });
